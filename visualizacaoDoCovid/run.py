97da6a7e03a2b0201a9fc28ecb3b7ae5e4567b0f
JeanCarlos2017 <jean__100limites@hotmail.com>
2020-07-10 18:55:21 -0300

    criando a pasta templates com os modelos do site, e definindo rotas para poder exibí-los

--- visualizacaoDoCovid/run.py
+++ visualizacaoDoCovid/run.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, make_response
 from flask_restful import Resource, Api
 
+
+#app = Flask('projetoVisualizacaoCovid') #usar o __name__ não é uma boa prática
 app = Flask(__name__)
 api = Api(app)
+
+
+@app.route("/flask") #funciona!
+def index():
+    return render_template("testModel.html")
 
 
 class HelloWorld(Resource):
@@ -10,7 +17,17 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 
-api.add_resource(HelloWorld, '/')
+class BubbleChart(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('testModel.html'), 200, headers)
+        #return render_template("index.html")
+        #return render_template("bbChart.html")
+        #return render_template('testModel.html')
+
+
+api.add_resource(HelloWorld, '/hello')
+api.add_resource(BubbleChart, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True)
