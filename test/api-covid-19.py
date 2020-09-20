81814af2706e533109bb09bec5021c8b63353213
JeanCarlos2017 <jean__100limites@hotmail.com>
2020-09-19 21:47:37 -0300

    Update api-covid-19.py
    buscando os dados de acordo com uma data específica

--- test/api-covid-19.py
+++ test/api-covid-19.py
@@ -1,4 +1,28 @@
+import requests
+from datetime import date,  timedelta, datetime
 
 
-dados_historicos_url = "https://api.apify.com/v2/datasets/3S2T1ZBxB9zhRJTBB/items?format=json&clean=1"
-dados_atual = "https://api.apify.com/v2/datasets/3S2T1ZBxB9zhRJTBB/items?format=json&clean=1"
+
+
+
+def retornaDados():
+    response = requests.get(URL_DADOS_ATUAL)
+    if response.status_code == 200:
+        return response.json()
+
+
+def buscaDadosPorData(data):
+    dados_covid = DADOS_COVID
+    for x in dados_covid:
+        if data in x.get('lastUpdatedAtSource'):
+            return x
+
+
+
+if __name__ == '__main__':
+    URL_DADOS_ATUAL = "https://api.apify.com/v2/datasets/3S2T1ZBxB9zhRJTBB/items?format=json&clean=1"
+    DADOS_COVID = retornaDados()
+    string_data_atual = date.today().strftime("%Y-%m-%d")
+    print(buscaDadosPorData(string_data_atual))
+
+
