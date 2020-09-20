efdeec12945a9d9265c61a5f8097090a1ef62579
JeanCarlos2017 <jean__100limites@hotmail.com>
2020-09-19 23:53:54 -0300

    Update api-covid-19.py
    adequando de acordo com a necessidades da api

--- test/api-covid-19.py
+++ test/api-covid-19.py
@@ -1,5 +1,6 @@
 import requests
-from datetime import date,  timedelta, datetime
+from datetime import date,  timedelta
+import datetime
 
 def retornaDados():
     response = requests.get(URL_DADOS_ATUAL)
@@ -9,34 +10,43 @@ def retornaDados():
 
 def buscaDadosPorData(data):
     dados_covid = DADOS_COVID
+    string_data = data.strftime("%Y-%m-%d")
     for x in dados_covid:
-        if data in x.get('lastUpdatedAtSource'):
+        if string_data in x.get('lastUpdatedAtSource'):
             return x
 
-def buscaDadosPorSemana(diaIni, dicionario, qntDias = 7):
-    #retorna a quantidade de infectados, mortos e recuperados da semana
-    ultimo_dia = diaIni + timedelta(days= qntDias)
-    string_ultimo_dia = ultimo_dia.strftime("%Y-%m-%d")
-    string_primeiro_dia = diaIni.strftime("%Y-%m-%d")
+def buscaDadosPorSemana(primeiro_dia, qntDias = 7):
+    #retorna a quantidade de infectados e mortos  da semana
+    dicionario = {'infected': 0, 'deceased': 0}
+    #caso a semana tenha se encerrado são 7 dias, do contrário ver quantos dias ainda restam
+    ultimo_dia = primeiro_dia + timedelta(days= qntDias)
     #obtem os dados do primeiro e ultimo dia da semana
-    dados_primeiro_dia = buscaDadosPorData(string_primeiro_dia)
-    dados_ultimo_dia = buscaDadosPorData(string_ultimo_dia)
+    dados_primeiro_dia = buscaDadosPorData(primeiro_dia)
+    if (dados_primeiro_dia == None):
+        #a API pula alguns dias por isso faço a verificação
+        return {'erro' : 'dia {} não encontrado na API nos desculpe por isso '.format(primeiro_dia.strftime("%Y-%m-%d"))}
+    dados_ultimo_dia = buscaDadosPorData(ultimo_dia)
+    if (dados_ultimo_dia == None):
+        return {'erro': 'dia {} não encontrado na API nos desculpe por isso '.format(ultimo_dia.strftime("%Y-%m-%d"))}
     for item in dicionario:
         #subtrai os dados do ultimo dia com o primeiro dia da semana para saber os dados desse período
         dados_semana = dados_ultimo_dia.get(item) - dados_primeiro_dia.get(item)
         dicionario[item] = dados_semana
-
     return dicionario
 
 
 if __name__ == '__main__':
     URL_DADOS_ATUAL = "https://api.apify.com/v2/datasets/3S2T1ZBxB9zhRJTBB/items?format=json&clean=1"
     DADOS_COVID = retornaDados()
-    string_data_atual = date.today().strftime("%Y-%m-%d")
-    # print(buscaDadosPorData(string_data_atual))
+    data_atual = datetime.datetime(2020, 6, 6)
+    if buscaDadosPorData(data_atual) == None:
+        print("lista vazia")
+    else:
+        print(buscaDadosPorData(data_atual))
     # testando pegar dados de uma semana do dia 13/09 ao 19/09
-    data_test_semana = datetime.strptime('2020-09-12', '%Y-%m-%d')
-    dicionario = {'infected': 0, 'deceased': 0, 'recovered': 0}
-    print(buscaDadosPorSemana(data_test_semana, dicionario))
+    data_test_semana = datetime.datetime(2020, 9, 12)
+    print(buscaDadosPorSemana(data_test_semana))
+
+
 
 
