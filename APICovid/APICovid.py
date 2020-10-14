bcf6f5083f05dc0e2e291ee981e3a0d06be02160
JeanCarlos2017 <jean__100limites@hotmail.com>
2020-10-14 17:57:35 -0300

    Update APICovid.py
    função buscaDadosDiaaDia foi refatorada

--- APICovid/APICovid.py
+++ APICovid/APICovid.py
@@ -7,22 +7,24 @@ class APICovid:
     def __init__(self):
         self.URL_API = "https://api.apify.com/v2/datasets/3S2T1ZBxB9zhRJTBB/items?format=json&clean=1"
         self.dados_covid_semanal={}
-        self.dados_todos_dias={}
+        self.dados_todos_dias={} #quantidade de casos novos dia a dia
         self.dados_covid = self.retornaDados()
-        self.dados_covid_refinado = {} #contendo apenas os infectados e mortos
+        self.dados_covid_refinado = {} #quantidade  acumulada dos infectados e mortos
 
     def retornaDados(self):
         response = requests.get(self.URL_API)
         if response.status_code == 200:
             return response.json()
 
-    def refinaDadosJSON(self, dados_diario):
+    def refinaDadosJSON(self, dados_diario, data):
         dicionario = {'infected': 0, 'deceased': 0}
         for item in dicionario:
             # print("item {} :: dados_item :: {}".format(item, dados_diario.get(item)))
             dicionario[item] = dados_diario.get(item)
+        dicionario['data'] = data
         return dicionario
 
+    #retorna em um dicionário a quantidade de infectados e mortos acumulados
     def retornaInfectadosEMortos(self):
         contador_dia = 0
         data_final = datetime.datetime.now()
@@ -30,7 +32,7 @@ class APICovid:
         while (data_final >= data_inicio):
             dados_diario = self.buscaDadosPorData(data_inicio)
             if dados_diario != None:
-                self.dados_covid_refinado[contador_dia] = self.refinaDadosJSON(dados_diario)
+                self.dados_covid_refinado[contador_dia] = self.refinaDadosJSON(dados_diario, data_inicio)
                 contador_dia+=1
             data_inicio += timedelta(days=1)
 
@@ -52,22 +54,17 @@ class APICovid:
 
    # retorna a quantidade de infectados e mortos de todos os dias
     def buscaDadosDiaDia(self):
-        data_final = datetime.datetime.now()
-        data_inicio = datetime.datetime(2020, 3, 14)
-        #data_inicio = datetime.datetime(2020, 10, 11)
-
-        #no dia 0 nao tem nenhum caso, é o início
+        self.retornaInfectadosEMortos() #dicionário a quantidade de infectados e mortos acumulados
         contador_dia = 0
-        self.dados_todos_dias[0] = {'infected': 0, 'deceased': 0}
+        quantidade_dias = self.dados_covid_refinado.__len__()
+        print('quantidade de dias :: {}'.format(quantidade_dias))
 
-        while (data_final >= data_inicio):
-            dados_ultimo_dia = self.buscaDadosPorData(data_inicio)
-            if dados_ultimo_dia != None:
-                contador_dia += 1
-                self.dados_todos_dias[contador_dia]=\
-                    self.subtracaoDadosDiasDiferentes(self.dados_todos_dias[contador_dia-1], dados_ultimo_dia)
-            data_inicio += timedelta(days=1)
-        return self.dados_todos_dias
+        while (contador_dia < quantidade_dias-1):
+            dados_anterior = self.dados_covid_refinado[contador_dia] # quantidade de infectados e mortos do dia anterior
+            contador_dia+=1
+            dados_atual = self.dados_covid_refinado[contador_dia] # quantidade de infectados e mortos do dia atual
+            self.dados_todos_dias[contador_dia-1] =\
+                    self.subtracaoDadosDiasDiferentes(dados_anterior, dados_atual)
 
     #mensagem de erro: dado nao encontrado
     def erroDiaNaoEncontrado(self, data):
@@ -116,9 +113,18 @@ class APICovid:
 
 if __name__ == '__main__':
     dadosAPI =  APICovid()
+    #retorna os dados de acordo com uma data
     #print(dadosAPI.getDadosPorData())
+
+    #retorna a quantidade de casos que ocorreu a cada semana
     #print(dadosAPI.buscaDadosTodasSemana())
     #print(dadosAPI.dados_covid)
-    #print(dadosAPI.buscaDadosDiaDia())
+
+    #retorna a quantidade de casos que ocorreu no dia
     dadosAPI.retornaInfectadosEMortos()
-    print(dadosAPI.dados_covid_refinado)
+    dadosAPI.buscaDadosDiaDia()
+    print(dadosAPI.dados_todos_dias)
+
+    #retorna os dados acumulados
+    #dadosAPI.retornaInfectadosEMortos()
+    #print(dadosAPI.dados_covid_refinado)
