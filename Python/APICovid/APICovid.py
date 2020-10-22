83a522cfc9c2a4e95de5afcee42904bc93ed1ec6
JeanCarlos2017 <jean__100limites@hotmail.com>
2020-10-22 19:40:15 -0300

    Update APICovid.py
    reformulando a função de buscaDadosTodasSemanas, tanto para melhorar o código como para melhorar os dados_semanal

--- Python/APICovid/APICovid.py
+++ Python/APICovid/APICovid.py
@@ -19,7 +19,6 @@ class APICovid:
     def refinaDadosJSON(self, dados_diario, data):
         dicionario = {'infected': 0, 'deceased': 0, 'data': data.strftime('%d/%m/%Y')}
         for item in dicionario:
-            # print("item {} :: dados_item :: {}".format(item, dados_diario.get(item)))
             if item != 'data':
                 dicionario[item] = dados_diario.get(item)
 
@@ -36,15 +35,6 @@ class APICovid:
                 self.dados_covid_refinado[contador_dia] = self.refinaDadosJSON(dados_diario, data_inicio)
                 contador_dia+=1
             data_inicio += timedelta(days=1)
-
-    #método para teste simples
-    def getDadosPorData(self):
-        data_atual = datetime.datetime(2020, 6, 9)
-        dados_data = self.buscaDadosPorData(data_atual)
-        if dados_data == None:
-            return self.erroDiaNaoEncontrado(dados_data)
-        else:
-            return dados_data
 
     #retorna dados da data especificada
     def buscaDadosPorData(self, data):
@@ -81,45 +71,38 @@ class APICovid:
         dicionario['data'] = dados_ultimo_dia['data']
         return dicionario
 
+    #retorn o dado de um dia específico do self.retornaInfectadosEMortos()
+    def buscaPorDataEmDadosRefinados(self, data):
+        for x in self.dados_covid_refinado:
+            if self.dados_covid_refinado[x]['data'] == data:
+                return self.dados_covid_refinado[x]
+        return None
+
     #retorna a quantidade de infectados e mortos na semana
-    def buscaDadosPorSemana(self, primeiro_dia_semana, qntDias=7):
-        # retorna a quantidade de infectados e mortos  da semana
-
-        # caso a semana tenha se encerrado são 7 dias, do contrário ver quantos dias ainda restam
-        ultimo_dia = primeiro_dia_semana + timedelta(days=qntDias)
-        # obtem os dados do primeiro e ultimo dia da semana
-        dados_primeiro_dia = self.buscaDadosPorData(primeiro_dia_semana)
-
-        if (dados_primeiro_dia == None):
-            return self.erroDiaNaoEncontrado(primeiro_dia_semana)
-        dados_ultimo_dia = self.buscaDadosPorData(ultimo_dia)
-
-        if (dados_ultimo_dia == None):
-            return self.erroDiaNaoEncontrado(ultimo_dia)
-        #compara a semana atual com a semana anterior para assim saber a quantidade de mostes e infectados que ocorreu na semana atual
-        dicionario = self.subtracaoDadosDiasDiferentes(dados_primeiro_dia, dados_ultimo_dia)
-        return dicionario
-
     def buscaDadosTodasSemana(self):
-        # retorna um dicionário com os dados de todas as semanas
-        data_final = datetime.datetime.now()
+        self.retornaInfectadosEMortos()  # dicionário a quantidade de infectados e mortos acumulados
         contador_semana = 12
+        data_final = datetime.datetime.now()
         data_inicio = datetime.datetime(2020, 3, 14)  # fim da primeira semana
         while (data_final >= data_inicio + timedelta(days=7)):
-            self.dados_covid_semanal[contador_semana] = self.buscaDadosPorSemana(data_inicio)
+            dados_anterior = self.buscaPorDataEmDadosRefinados(data_inicio.strftime('%d/%m/%Y'))
             data_inicio += timedelta(days=7)
-            contador_semana += 1
-        return self.dados_covid_semanal
+            dados_atual =  self.buscaPorDataEmDadosRefinados(data_inicio.strftime('%d/%m/%Y'))
+            if dados_anterior == None or dados_atual == None:
+                self.dados_covid_semanal[contador_semana] = None
+            else:
+                self.dados_covid_semanal[contador_semana] = self.subtracaoDadosDiasDiferentes(dados_anterior, dados_atual)
+            contador_semana+=1
 
 
 if __name__ == '__main__':
-    dadosAPI = APICovid()
+     dadosAPI = APICovid()
     #retorna os dados de acordo com uma data
     #print(dadosAPI.getDadosPorData())
 
     #retorna a quantidade de casos que ocorreu a cada semana
-    #print(dadosAPI.buscaDadosTodasSemana())
-    #print(dadosAPI.dados_covid)
+    print(dadosAPI.buscaDadosTodasSemana())
+    print((dadosAPI.dados_covid_semanal))
 
     #retorna a quantidade de casos que ocorreu no dia
     dadosAPI.retornaInfectadosEMortos()
